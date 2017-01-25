ac7a6ceb1b6f2e47b9f7cb85645797a96c1cb018
begibeggineta <bergthorbi223@nemi.tskoli.is>
2017-01-25 11:22:54 +0000

    Kláraði Dæmi 3

--- verkefni.py
+++ verkefni.py
@@ -1,8 +1,18 @@
-#Liður 1
-
-tala1 = int(input("Sláðu inn tölu"))
-tala2 = int(input("Sláðu inn aðra tölu"))
-marg = tala1 + tala2
-bla = tala1 * tala2
-print("tölurnar plúsaðar",tala1,"+", tala2,"=", marg)
-print("tölurnar sinnumaðar",tala1,"*", tala2,"=", bla)
+#Liður 3
+print("#Liður 3")
+texti = input("Settu inn random texta")
+telhast = 0
+tellagst = 0
+telefthst = 0
+sidastur = False
+for i in range(len(texti)):
+    if texti[i].isupper() and texti[i]:
+        telhast += 1
+        sidastur = True
+    elif texti[i].islower() and sidastur == True:
+        telefthst += 1
+        sidastur = True
+    if texti[i].islower():
+        tellagst += 1
+        sidastur = False
+print("í þessum texta eru ", telhast," hástafir", tellagst, "Lágstafir", telefthst," lágstafir sem koma strax eftir hástöfum")
