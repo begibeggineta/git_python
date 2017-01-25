1d79f34d67568cc6856da06a8eb6be2b6dc3fb2a
begibeggineta <bergthorbi223@nemi.tskoli.is>
2017-01-25 11:23:21 +0000

    Kláraði Dæmi 2

--- verkefni.py
+++ verkefni.py
@@ -1,18 +1,5 @@
-#Liður 3
-print("#Liður 3")
-texti = input("Settu inn random texta")
-telhast = 0
-tellagst = 0
-telefthst = 0
-sidastur = False
-for i in range(len(texti)):
-    if texti[i].isupper() and texti[i]:
-        telhast += 1
-        sidastur = True
-    elif texti[i].islower() and sidastur == True:
-        telefthst += 1
-        sidastur = True
-    if texti[i].islower():
-        tellagst += 1
-        sidastur = False
-print("í þessum texta eru ", telhast," hástafir", tellagst, "Lágstafir", telefthst," lágstafir sem koma strax eftir hástöfum")
+#liður 2
+print("#Liður 2")
+ta1 = input("gefðu mér Fornafnið þitt")
+ta2 = input("gefðu mér eftirnafnið þitt")
+print("hello", ta1, ta2)
